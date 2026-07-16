2201520e4266d2fdefbc7b642fe28bed917fb085
Rubicones <dmitriypopov1543@gmail.com>
2026-07-17 01:04:38 +0200

    redeploy

--- app/band/layout.tsx
+++ app/band/layout.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from 'react'
 import { noIndexMetadata } from '@/lib/seo'
 
 export const metadata = noIndexMetadata('Band workspace')
-//here 
 export default function BandLayout({ children }: { children: ReactNode }) {
   return children
 }
